c9645275552321846f551c8ca6689e75e8498b20
sankar <ballasankarreddy@gmail.com>
2019-02-09 22:11:38 +0530

    List Example class added

--- ExcelDataUpload/src/main/java/com/spring/excel/ExcelDataUploadApplication.java
+++ ExcelDataUpload/src/main/java/com/spring/excel/ExcelDataUploadApplication.java
@@ -3,11 +3,15 @@ package com.spring.excel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.spring.excel.collection.ExampleOFArrayList;
+
 @SpringBootApplication
 public class ExcelDataUploadApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(ExcelDataUploadApplication.class, args);
+		System.out.println(ExampleOFArrayList.getList());
 	}
 
 }
---
+++ ExcelDataUpload/src/main/java/com/spring/excel/collection/ExampleOFArrayList.java
@@ -0,0 +1,18 @@
+package com.spring.excel.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExampleOFArrayList {
+	
+	public static List<String> getList() {
+		List<String> list=new ArrayList<String>();
+		list.add("Sankar");
+		list.add("Sankar");
+		list.add("Sankar");
+		
+		return list;
+		
+	}
+
+}
